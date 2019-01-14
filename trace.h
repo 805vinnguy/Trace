f634d81ee9a03d3f7458fb303b1a18762b9ac583
Vinh Nguyen <805vinnguy@gmail.com>
2019-01-14 14:35:56 -0800

    2 wireshark captures, eth, arp, ip, tcp, udp header structs

--- trace.h
+++ trace.h
@@ -6,13 +6,23 @@
 #include <stdint.h>
 
 struct ethernet 
-{
-    char c;
+{/* 14 bytes */
+    uint8_t src[6];            /* source MAC */
+    uint8_t dst[6];            /* destination MAC */
+    uint16_t next_protocol;
 }__attribute__((packed));
 
 struct arp
-{
-    char c;
+{/* 28 bytes */
+    uint16_t htype;            /* hardware type */
+    uint16_t ptype;            /* protocol type */
+    uint8_t hlen;              /* hardware address length */
+    uint8_t plen;              /* protocol address length */
+    uint16_t oper;             /* operation */
+    uint8_t sha[6];            /* sender hardware address */
+    uint8_t spa[4];            /* sender protocol address */
+    uint8_t tha[6];            /* target hardware address */
+    uint8_t tpa[4];            /* target protocol address */
 }__attribute__((packed));
 
 struct ip
@@ -23,7 +33,7 @@ struct ip
     uint16_t id;               
     uint16_t flags_frag;
     uint8_t TTL;               /* time to live */
-    uint8_t protocol;          /* next protocol */
+    uint8_t next_protocol;
     uint16_t checksum;
     uint32_t src;              /* sender ip */
     uint32_t dst;              /* receiver ip */
@@ -31,13 +41,24 @@ struct ip
 }__attribute__((packed));
 
 struct tcp
-{
-    char c;
+{/* 20 bytes min */
+    uint16_t src_port;         /* sending port */
+    uint16_t dst_port;         /* receiving port */
+    uint32_t sequence;         /* sequence number */
+    uint32_t ack;              /* acknowledgment number */
+    uint16_t offset_res_flags; /* data offset(4) reserved(3) flags(9) */
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urg;              /* urgent pointer */
+    /* uint32_t options; */
 }__attribute__((packed));
 
 struct udp
-{
-    char c;
+{/* 8 bytes */
+    uint16_t src_port;         /* sending port */
+    uint16_t dst_port;         /* destination port */
+    uint16_t length;           /* UDP header + data (bytes) */
+    uint16_t checksum;
 }__attribute__((packed));
 
 
