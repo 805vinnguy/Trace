7c102240c11d6e6197751f850044f03b1644a3b4
Vinh Nguyen <805vinnguy@gmail.com>
2019-01-14 16:30:08 -0800

    after installing shared libraries, must call ldconfig; pcap_open_offline() successful

--- trace.c
+++ trace.c
@@ -1,6 +1,11 @@
 #include "trace.h"
 
 int main(int argc, char* argv[]) {
-    unsigned char* frame_buf = NULL;
+    unsigned char frame_buf[PCAP_ERRBUF_SIZE];
+    pcap_t* tracefile = pcap_open_offline(argv[1], (char*)frame_buf);
+    if(tracefile == NULL) {
+        fprintf(stderr, "%s\n", frame_buf);
+        exit(EXIT_FAILURE);
+    }
     exit(EXIT_SUCCESS);
 }
